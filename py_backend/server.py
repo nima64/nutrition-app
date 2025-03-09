142b797aed15e87543f0be718351acb002cf957a
nima64 <kennethe407@gmail.com>
2025-03-08 16:19:06 -0800

    py_backend:server.py moved user print data into a function

--- py_backend/server.py
+++ py_backend/server.py
@@ -3,19 +3,19 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 
-cred = credentials.Certificate("nutrition-app-a791d-firebase-adminsdk-fbsvc-0bdf656448.json")
+def printUserData(db):
+    ref = db.reference()  # Get a reference to the root of your database
+    print(ref)
+    users = ref.child("users").get()
+    print(users)  #This will print a dictionary of users.
+    for user_id, user_data in users.items():
+       print(f"User ID: {user_id}, Data: {user_data}")
+
+
+cred = credentials.Certificate("nutrition-app-a791d-firebase-adminsdk-fbsvc-fea7e0cb21.json")
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://nutrition-app-a791d-default-rtdb.firebaseio.com/'  # Replace with your database URL
 })
-
-# Now you can access your Realtime Database
-ref = db.reference()  # Get a reference to the root of your database
-print(ref)
-users = ref.child("users").get()
-print(users)  #This will print a dictionary of users.
-# for user_id, user_data in users.items():
-#    print(f"User ID: {user_id}, Data: {user_data}")
-# ... your code to read or write data ...
 
 
 
